2e64769f7cea83510bda042bdabf23db4b1d6a62
Andrey Konopkov <konopkov@gmail.com>
2017-10-06 01:17:32 +0300

    refactoring: BooksGrid as Stateless Functional Component

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -1,22 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Book from './Book'
 
-class BooksGrid extends Component {
+const BooksGrid = (props) => {
 
-    render() {
+    const books = props.books;
 
-        const books = this.props.books;
-
-        return (
-            <ol className="books-grid">
-                {books.map((book) => (
-                    <li key={book.id}>
-                        <Book book={book} onHandleChange={this.props.onHandleChange}/>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
+    return (
+        <ol className="books-grid">
+            {books.map((book) => (
+                <li key={book.id}>
+                    <Book book={book} onHandleChange={props.onHandleChange}/>
+                </li>
+            ))}
+        </ol>
+    )
+};
 
 export default BooksGrid
