29dddb85df615be8944b64c4af66f2b7f45b747d
Andrey Konopkov <konopkov@gmail.com>
2017-10-05 23:07:04 +0300

    fix: Book cover dynamic height and width

--- src/Book.js
+++ src/Book.js
@@ -11,8 +11,8 @@ class Book extends Component {
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{
-                        width: 128,
-                        height: 193,
+                        width: '100%',
+                        height: '100%',
                         backgroundImage: `url(${book.imageLinks ? book.imageLinks.smallThumbnail : ''})`
                     }}> </div>
                     <div className="book-shelf-changer">
