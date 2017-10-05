64cd2feb547d9f3e88bbfa61f1c28d83948e32ab
Andrey Konopkov <konopkov@gmail.com>
2017-10-06 01:14:21 +0300

    refactoring: ListBooks as Stateless Functional Component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,44 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
+const ListBooks = (props) => {
 
-    render() {
+    const books = props.books;
+    const shelves = [{
+        title: 'Currently Reading',
+        name: 'currentlyReading'
+    }, {
+        title: 'Want to read',
+        name: 'wantToRead'
+    }, {
+        title: 'Read',
+        name: 'read'
+    }];
 
-        const books = this.props.books;
-        const shelves = [{
-            title: 'Currently Reading',
-            name: 'currentlyReading'
-        }, {
-            title: 'Want to read',
-            name: 'wantToRead'
-        }, {
-            title: 'Read',
-            name: 'read'
-        }];
-
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        {shelves.map((shelf) => (
-                            <BookShelf key={shelf.name}
-                                       shelfTitle={shelf.title}
-                                       books={books.filter((book) => book.shelf === shelf.name)}
-                                       onHandleChange={this.props.onHandleChange}/>
-                        ))}
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add book</Link>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    {shelves.map((shelf) => (
+                        <BookShelf key={shelf.name}
+                                   shelfTitle={shelf.title}
+                                   books={books.filter((book) => book.shelf === shelf.name)}
+                                   onHandleChange={props.onHandleChange}/>
+                    ))}
                 </div>
             </div>
-        )
-    }
-}
+            <div className="open-search">
+                <Link to='/search'>Add book</Link>
+            </div>
+        </div>
+    )
+};
 
 export default ListBooks
